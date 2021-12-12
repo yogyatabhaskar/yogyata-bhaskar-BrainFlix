17311a1914e9c8c25d367e2c6b1696228c502ca2
Bhaskar <karanbhaskar@Bhaskars-MacBook-Pro.local>
2021-12-11 19:55:35 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+import Header from './components/Header/Header';
+import Body from './components/Main/Main';
+// import videoDetailsData from '././data/video-details.json';
+// import videosData from '././data/videos.json';
+// import { Component } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    <Body />
+    </div>
+  );
+}
+
+export default App;
