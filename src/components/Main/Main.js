211daa84189ad1221a6aad38f7d9ad2a4ef54dd4
Bhaskar <karanbhaskar@Bhaskars-MacBook-Pro.local>
2021-12-12 20:09:14 -0500

    updated comment-section styling

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,49 +1,53 @@
 import "./Main.scss";
-import Hero from './Hero/Hero';
-import MainBody from './MainBody/MainBody';
-import videoDetailsData from '../../data/video-details.json';
-import AddComments from './MainBody/AddComments/AddComments'
-import videosData from '../../data/videos.json';
-import { Component } from 'react';
-import VideoRecommendation from './VideoSec/VideoSec';
-
+import Hero from "./Hero/Hero";
+import MainBody from "./MainBody/MainBody";
+import videoDetailsData from "../../data/video-details.json";
+import AddComments from "./MainBody/AddComments/AddComments";
+import videosData from "../../data/videos.json";
+import { Component } from "react";
+import VideoRecommendation from "./VideoSec/VideoSec";
 
 class Body extends Component {
-    state = {
-      videos: videosData,
-      selectedVideo: videoDetailsData[0]
-    }
+  state = {
+    videos: videosData,
+    selectedVideo: videoDetailsData[0],
+  };
 
-    render() {
-  
-      return(
-        <div className="home-page">
-          <Hero selectedVideo={this.state.selectedVideo}/>
-          <div className="home-page__description">
-            <MainBody selectedVideo={this.state.selectedVideo} /> 
-            {this.state.selectedVideo.comments.map((article) => {
-              return <AddComments key={article.id} 
-              name={article.name} 
-              date={article.timestamp} 
-              comment={article.comment}/>;
-            })}
+  render() {
+    return (
+      <div className="home-page">
+        <Hero selectedVideo={this.state.selectedVideo} />
+        
+        <div className="home-page__description">
+          <MainBody selectedVideo={this.state.selectedVideo} />
           
-          </div>
-          
-          <div className="home-page__recommendations">
-            {this.state.videos.map((video) => {
-              return <VideoRecommendation key={video.id} 
-              image={video.image} 
-              title={video.title} 
-              channel={video.channel}/>;
-            })}
-  
-          </div>
-           
-            
+          {this.state.selectedVideo.comments.map((article) => {
+            return (
+              <AddComments
+                key={article.id}
+                name={article.name}
+                date={article.timestamp}
+                comment={article.comment}
+              />
+            );
+          })}
         </div>
-      );
-    }
+
+        <div className="home-page__recommendations">
+          {this.state.videos.map((video) => {
+            return (
+              <VideoRecommendation
+                key={video.id}
+                image={video.image}
+                title={video.title}
+                channel={video.channel}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
   }
+}
 
 export default Body;
--- src/components/Main/MainBody/AddComments/AddComments.js
+++ src/components/Main/MainBody/AddComments/AddComments.js
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
-import mohan from '../../../../assets/Images/Mohan-muruge.jpg';
-import './AddComments.scss';
+import mohan from "../../../../assets/Images/Mohan-muruge.jpg";
+import "./AddComments.scss";
 
 export default class AddComments extends Component {
-    render() {
-        return (
-            <div className="comment-list">
-                <img className="comment-list__thumbnail" src={mohan} alt="grey-thumbnail"/>
-                <div className="comment-list__post">
-                <div className="comment-list__commentbox">
-                    <p className="comment-list__name">{this.props.name}</p>
-                    <p className="comment-list__date">{Date(this.props.timestamp)}</p> 
-                    {/* new Date(props.timestamp).toLocaleDateString() */}
-                </div>
+  render() {
+    return (
+      <div className="comment-list">
+        <div className="comment-list__thumbnail"></div>
+        <div className="comment-list__post">
+          <div className="comment-list__commentbox">
+            <p className="comment-list__name">{this.props.name}</p>
+            <p className="comment-list__date">{Date(this.props.timestamp)}</p>
+            {/* new Date(props.timestamp).toLocaleDateString() */}
+          </div>
 
-                <p className="comment-list__comment">{this.props.comment}</p>
-                </div> 
-                
-            </div>
-        );
-    }
+          <p className="comment-list__comment">{this.props.comment}</p>
+        </div>
+      </div>
+    );
+  }
 }
