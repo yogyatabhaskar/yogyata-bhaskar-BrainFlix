5b7a8bf51d3649f0762b2a6ad76282b78296e3ed
Bhaskar <karanbhaskar@Bhaskars-MacBook-Pro.local>
2021-12-18 05:38:42 -0500

    Updated upload-page

--- src/components/UploadBody/UploadBody.js
+++ src/components/UploadBody/UploadBody.js
@@ -1,40 +1,33 @@
 import React from 'react';
 import "./UploadBody.scss"
+import imagepath from '../../assets/Images/Upload-video-preview.jpg';
 
 export default function VideoUpload() {
-    let imagePath = '../../assets/images/Upload-video-preview.jpg';
 
     return (
         // Video Upload section
-        <section className="videoupload">
-            <h2 className="videoupload__title">Upload Video</h2>
-            <div className="videoupload__main">
-                <div className="videoupload__main-left">
-                    <h3 className="videoupload__thumbnail-title">VIDEO THUMBNAIL</h3>
-                    <div className="videoupload__thumbnail" style={{ backgroundImage: `url(${imagePath})` }}></div>
+        <div className="upload">
+                <h1 className= "upload__header">Upload Video</h1>
+                <div className="upload__border">
+                    <section className="upload__hero">
+                        
+                        <p className="upload__title">VIDEO THUMBNAIL</p>
+                        <img className="upload__video" src={imagepath} alt="video-thumnail"/>
+                    </section>
+                    <section className="upload-input">
+                        <div className="upload-input__border">
+                            <p className="upload-input__title">TITLE YOUR VIDEO</p>
+                            <input className="upload-input__input1" placeholder="Add a title to your video"></input>
+                            <p className="upload-input__title">ADD A VIDEO DESCRIPTION</p>
+                            <textarea className="upload-input__input2" placeholder="Add a description of your video"></textarea>
+                        </div>
+                    </section>
                 </div>
-                <div className="videoupload__main-right">
-                    <form className="videoupload__form" id="videoupload_form" action="">
-                        <label className="videoupload__form-label--title" htmlFor="videouploadTitle">TITLE YOUR VIDEO</label>
-                        <input
-                            className="videoupload__form-input"
-                            type="text"
-                            name="videouploadTitle"
-                            id="videouploadTitle"
-                            placeholder="Add a title to your video"></input>
-                        <label className="videoupload__form-label--description" htmlFor="videouploadDesc">ADD A VIDEO DESCRIPTION</label>
-                        <textarea
-                            className="videoupload__form-textarea"
-                            rows="7" cols="35"
-                            name="videouploadDesc" id="videouploadDesc"
-                            placeholder="Add a description of your video"></textarea>
-                    </form>
+                <div className="upload-input__buttons">
+                    {/* <button className="upload-input__buttons__cancel-button1">CANCEL</button> */}
+                    <button className="upload-input__buttons__publish-button">PUBLISH</button>
+                    <button className="upload-input__buttons__cancel-button2">CANCEL</button>
                 </div>
-            </div>
-            <div className="videoupload__button-container">
-                <button className="videoupload__publish-btn" id="videoupload__publish-btn">PUBLISH</button>
-                <button className="videoupload__cancel-btn" id="videoupload__cancel-btn">CANCEL</button>
-            </div>
-        </section>
+            </div>  
     );
 }
