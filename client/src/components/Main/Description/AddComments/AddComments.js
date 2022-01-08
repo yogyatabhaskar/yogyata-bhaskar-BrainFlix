325f264df421527168de33d3fc2aba8743744d75
Bhaskar <karanbhaskar@Bhaskars-MacBook-Pro.local>
2022-01-08 18:42:32 -0500

    Updated the server and client

--- client/src/components/Main/Description/AddComments/AddComments.js
+++ client/src/components/Main/Description/AddComments/AddComments.js
@@ -8,7 +8,7 @@ export default class Comments extends React.Component {
               const dateElem = new Date(item.timestamp);
                 const dateElement = dateElem.toLocaleDateString();
                 return (
-                    <section className="comment-list" key={item.id}>
+                    <section className="comment-list" key={item.timestamp}>
                         <div className="comment-list__thumbnail"></div>
                         <div className="comment-list__post">
                             <div className="comment-list__commentbox">
@@ -23,31 +23,3 @@ export default class Comments extends React.Component {
         )
     }
 }
-
-
-
-
-
-
-// class AddComm extends React.Component ({selectedVideo}) {
-//     render () {
-//         return(
-//             selectedVideo.map( (item)=> {
-//                 return (
-//                     <section className="comments" key={item.id}>
-//                         <img className="comment-list__thumbnail" alt=""></img>
-//                         <div className="comment-list__post">
-//                             <div className="comment-list__commentbox">
-//                                 <p className="comment-list__name">{item.name}</p>
-//                                 <p className="comment-list__date">{item.date}</p>
-//                             </div>
-//                             <p className="comment-list__comment">{item.comment}</p>
-//                         </div>
-//                     </section>
-//                 )
-//             })
-//         )
-//     }
-// }
-    
-// export default AddComm;
